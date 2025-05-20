230d369c792e14d520799b2559fe18154e4ffe48
sanikaa-16 <116032220+sanikaa-16@users.noreply.github.com>
2025-05-20 21:41:41 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,39 @@
-import { useAVToggle } from "@100mslive/react-sdk";
+import { useAVToggle, useHMSActions } from "@100mslive/react-sdk";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMicrophone,
+  faMicrophoneSlash,
+  faVideo,
+  faVideoSlash,
+  faPhoneSlash,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-  const { isLocalAudioEnabled, toggleAudio, isLocalVideoEnabled, toggleVideo } =
-    useAVToggle();
+  const {
+    isLocalAudioEnabled,
+    toggleAudio,
+    isLocalVideoEnabled,
+    toggleVideo,
+  } = useAVToggle();
+  const hmsActions = useHMSActions();
+
+  const handleLeave = () => {
+    hmsActions.leave(); // Leave the call
+  };
+
   return (
     <div className="control-bar">
       <button className="btn-control" onClick={toggleAudio}>
-        {isLocalAudioEnabled ? "Mute" : "Unmute"}
+        <FontAwesomeIcon icon={isLocalAudioEnabled ? faMicrophone : faMicrophoneSlash} />
       </button>
       <button className="btn-control" onClick={toggleVideo}>
-        {isLocalVideoEnabled ? "Hide" : "Unhide"}
+        <FontAwesomeIcon icon={isLocalVideoEnabled ? faVideo : faVideoSlash} />
+      </button>
+      <button className="btn-control leave-btn" onClick={handleLeave}>
+        <FontAwesomeIcon icon={faPhoneSlash} /> {/* 🚫 Leave call */}
       </button>
     </div>
   );
 };
+
 export default Footer;
